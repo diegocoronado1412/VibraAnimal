e47508ec9173eeb6175d80d22b9ed53436ad689d
Laura Ceballos <sofia12cq@gmail.com>
2025-02-18 15:11:48 -0500

    Segundo Commit

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///vibra_animal.db'  # Puedes cambiarlo a PostgreSQL o MySQL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)  # Para manejar sesiones seguras
+
